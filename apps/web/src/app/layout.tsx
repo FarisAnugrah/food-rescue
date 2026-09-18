a10f1b5962c38081b54080c39db5387272cbe98c
FarisAnugrah <farisakun65@gmail.com>
2026-09-18 15:05:50 +0700

    fix: load leaflet CSS via CDN to fix broken map layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="id" className={`${geist.variable} h-full antialiased`}>
+      <head>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossOrigin="" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
--- apps/web/src/components/map-view.tsx
+++ apps/web/src/components/map-view.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import Link from "next/link";
 import { formatCurrency } from "@food-rescue/shared";
